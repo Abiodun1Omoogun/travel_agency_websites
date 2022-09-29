8144b4ef84678e2f8e844f843100558c25b885f1
Emmanuel Omoogun <abiodunebun12@gmail.com>
2022-09-29 11:46:28 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        black: '#000000',
+        gray: 'gray',
+        transparent: 'trannsparent',
+        white: '#FFFFFF',
+        clifford: '#027AFD',
+        lightcyan: '#027AFDE5',
+        blue: 'rgba(2, 72, 253, 0.81)'
+      },
+      fontFamily: {
+        'serif': ['Montserrat', 'san-serif'],
+      },
+      
+    }
+  }
+} 
